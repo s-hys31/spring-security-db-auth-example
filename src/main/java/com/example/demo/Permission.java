634acaaacbafb254c4b01432e333d26d17070cbb
s-hys31 <34961313+s-hys31@users.noreply.github.com>
2022-06-08 10:21:13 +0900

    create Permission Entity

--- src/main/java/com/example/demo/Permission.java
+++ src/main/java/com/example/demo/Permission.java
@@ -1,5 +1,41 @@
 package com.example.demo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Permission {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Permission(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Permission() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
