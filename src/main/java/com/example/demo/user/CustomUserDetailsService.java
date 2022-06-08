594adda0b37899e1c6d0af36e3b8430a9974ce95
s-hys31 <34961313+s-hys31@users.noreply.github.com>
2022-06-08 18:29:03 +0900

    add registerUser() in CustomUserDetailsService

--- src/main/java/com/example/demo/user/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/user/CustomUserDetailsService.java
@@ -1,7 +1,10 @@
 package com.example.demo.user;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,12 +13,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -28,6 +37,27 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .map(permission -> new SimpleGrantedAuthority(permission.getName())).collect(Collectors.toList());
 
         return new CustomUserDetails(user, authorities);
+    }
+
+    @Transactional
+    public void registerUser(String username, String password) {
+        userRepository.save(buildUser(username, password, "USER"));
+    }
+
+    private User buildUser(String username, String password, String... roleNames) {
+        var user = new User();
+        user.setName(username);
+        user.setPassword(password);
+        user.setEnabled(true);
+
+        var roles = new ArrayList<Role>();
+        for (String roleName : roleNames) {
+            roles.add(roleRepository.findByName(roleName));
+        }
+
+        user.setRoles(roles);
+
+        return user;
     }
 
 }
