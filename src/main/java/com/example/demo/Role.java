b95eb1fe297fd26467ad1bf5ab3abe3585951aad
s-hys31 <34961313+s-hys31@users.noreply.github.com>
2022-06-08 10:03:25 +0900

    create Role Entity

--- src/main/java/com/example/demo/Role.java
+++ src/main/java/com/example/demo/Role.java
@@ -1,8 +1,56 @@
 package com.example.demo;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Role {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private int name;
+
+    @ManyToMany
+    private List<Permission> permissions;
+
+    public Role(int id, int name, List<Permission> permissions) {
+        this.id = id;
+        this.name = name;
+        this.permissions = permissions;
+    }
+
+    public Role() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
 }
