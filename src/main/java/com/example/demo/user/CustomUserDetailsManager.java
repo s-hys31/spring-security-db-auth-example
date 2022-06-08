5221b0415939c49c60f64d5edb97829071e6a543
s-hys31 <34961313+s-hys31@users.noreply.github.com>
2022-06-08 17:23:23 +0900

    add interfaces in CustomUserDetailsManager

--- src/main/java/com/example/demo/user/CustomUserDetailsManager.java
+++ src/main/java/com/example/demo/user/CustomUserDetailsManager.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import com.example.demo.repository.UserRepository;
 
-public class CustomUserDetailsManager implements UserDetailsService {
+public class CustomUserDetailsManager implements UserDetailsManager, UserDetailsPasswordService {
 
     @Autowired
     private UserRepository userRepository;
@@ -28,6 +29,42 @@ public class CustomUserDetailsManager implements UserDetailsService {
                 .map(permission -> new SimpleGrantedAuthority(permission.getName())).collect(Collectors.toList());
 
         return new CustomUserDetails(user, authorities);
+    }
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
