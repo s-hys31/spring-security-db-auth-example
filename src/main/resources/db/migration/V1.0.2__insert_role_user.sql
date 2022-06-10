ac79931a6938cf3843aa394ea5261090d9ee29b6
s-hys31 <34961313+s-hys31@users.noreply.github.com>
2022-06-10 11:06:38 +0900

    add migration sql for insert user role

---
+++ src/main/resources/db/migration/V1.0.2__insert_role_user.sql
@@ -0,0 +1,12 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1,	'USER');
+
+INSERT INTO `role_permissions` (`role_id`, `permissions_id`) VALUES
+(1,	1);
